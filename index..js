c77e51e77a5d112b0a21c3e9004b1690602cf82a
Anna-Miron <ana04miron@gmail.com>
2024-10-06 13:01:13 +0100

    add variables

---
+++ index..js
@@ -0,0 +1,46 @@
+// Declare a variable for your age and display it in the console.
+let age = 27;
+console.log(age);
+
+// Calculate the sum of two numbers and display the result.
+let num1 = 8;
+let num2 = 8;
+let sum = num1 + num2;
+console.log(sum);
+
+// Declare a variable for the temperature in Celsius and convert it to Fahrenheit.
+let celsiusTemperature = 28;
+let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+console.log(fahrenheitTemperature);
+
+// Assign a string with the value "Hello, " and add your name, then display the result.
+let greeting = "Hello, ";
+let myName = "Anna";
+let fullGreeting = greeting + myName;
+console.log(fullGreeting);
+
+// Check if a number is even or odd and display the result.
+let numberToCheck = 5;
+let isEven = numberToCheck % 2 === 0;
+console.log("The number is even: ", isEven);
+
+// Declare a boolean variable to check if you are a student or not.
+let isStudent = true;
+console.log(isStudent);
+
+// Declare two string variables and concatenate them.
+let firstName = "Anna";
+let lastName = "Miron";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Calculate the remainder of the division of two numbers and display the result.
+let dividend = 17;
+let divisor = 5;
+let remainder = dividend % divisor;
+console.log(remainder);
+
+// Declare a variable and assign it the value undefined, then display the result.
+let undefinedVariable;
+console.log(undefinedVariable);
+
